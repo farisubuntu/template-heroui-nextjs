8cdd2444bf6036e1c16985360d07e07d2383b8fa
farisubuntu <farisubunut@gmail.com>
2025-04-16 23:03:35 +0300

    update...

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,4 +1,9 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import { Fira_Code as FontMono,
+   Inter as FontSans,
+   Noto_Naskh_Arabic as FontNaskh,
+   Noto_Nastaliq_Urdu as FontNastaliq
+
+  } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -8,4 +13,14 @@ export const fontSans = FontSans({
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+});
+
+export const fontNaskh = FontNaskh({
+  subsets: ["arabic"],
+  variable: "--font-naskh",
+});
+
+export const fontNastaliq = FontNastaliq({
+  subsets: ["arabic"],
+  variable: "--font-nastaliq",
 });
---
+++ zNotes.md
@@ -0,0 +1,9 @@
+# zNotes
+
+## `heroUI`
+
+### Important Points
+
+- **Component slots**: To provide maximum flexibility, many `HeroUI` components have **slots**, allowing developers **_to inject custom styles or content in specific areas of a component._** Each slot can be individually styled, offering granular control over the appearance and behavior of the component.
+- **Components API**
+- requires `Framer Motion 11.9` or later.
